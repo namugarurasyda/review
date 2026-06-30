d140afbd85d75c401e9eb86333ebb43ad33e9562
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-06-30 23:53:12 +0300

    Changes

--- src/Components/Trial.jsx
+++ src/Components/Trial.jsx
@@ -3,6 +3,14 @@ import react, {useState, useEffect} from 'react';
 export function Trial(){
     const [count, setCount] = useState(0);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCount(count + 1);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [count]);
+
 return (
     <div>
         <h4>Trial Component</h4>
