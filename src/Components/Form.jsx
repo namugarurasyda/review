33b095d804cba56e49be90c7426163bf4adb2017
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-01 23:55:08 +0300

    Setting state

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>
+  }
