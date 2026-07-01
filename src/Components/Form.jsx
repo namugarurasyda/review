b4146d43f61ef308a0bb988aa95d92c1e2e94625
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-02 00:14:47 +0300

    Adding the Submit form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -55,5 +55,20 @@ export default function Form() {
     </>
   );
 
-  
+
 }
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
+
