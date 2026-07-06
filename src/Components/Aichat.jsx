a7df055e087ad0c1331133b5f863ef5024031d8e
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-06 23:46:40 +0300

    Changed b

--- src/Components/Aichat.jsx
+++ src/Components/Aichat.jsx
@@ -14,6 +14,7 @@ import { Outlet, Link } from 'react-router-dom';
   useEffect(() => { 
      const outputDiv = document.getElementById('chat-output');
     outputDiv.innerText = ""; // Clear previous output
+    
   })
 
 
