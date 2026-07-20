ef6a64538bd1d745f307bad221ffdc94f6d4cd15
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-20 23:15:27 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -92,4 +92,13 @@ function App() {
   }
 }
 
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Philosopher’s Path', done: true },
+  { id: 1, text: 'Visit the temple', done: false },
+  { id: 2, text: 'Drink matcha', done: false }
+];
+
+}
+
 export default App
