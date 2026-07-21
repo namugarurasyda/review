d11c4b7ff15962f5505b62a241c4dc93672c6ec7
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-21 23:55:06 +0300

    Completing the context

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,4 +1,11 @@
 import { useContext } from 'react';
 
 function MyComponent() {
-  const theme = useContext(ThemeContext);}
+  const theme = useContext(ThemeContext); 
+
+   return (
+    <ThemeContext value="dark">
+      <Form />
+    </ThemeContext>
+  );
+}
