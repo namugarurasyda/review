3f163add45155d097ffa40c5a4fce780993ef778
namugarurasyda <2100813912@std.kyu.ac.ug>
2026-07-22 00:00:21 +0300

    Completing the Form

--- src/Components/Addit.jsx
+++ src/Components/Addit.jsx
@@ -18,3 +18,14 @@ function Form() {
     </Panel>
   );
 }
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = 'panel-' + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  )
+}
